e5214d8813be81822fa9f0d25d091633e4097226
unknown <zhangyunan@nuctech.com>
2019-07-06 11:44:00 +0800

    use CLR to adjust lr

--- bayesian_segnet_basic.py
+++ bayesian_segnet_basic.py
@@ -2,12 +2,11 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-import tf_unpool
 import pooling
 
 
 
-class BayesianSegNetBasic(object):
+class SegNetBasic(object):
 
 	def __init__(self, input_img, num_classes):
 		self.X = input_img
@@ -24,7 +23,6 @@ class BayesianSegNetBasic(object):
 		# activation
 		features = keras.layers.Activation('relu')(bn)
 		# 1st pool
-		#features, mask1 = tf_unpool.max_pool_with_argmax(features, 2)
 		features, mask1 = pooling.max_pool_with_argmax(net=features, ksize=[1,2,2,1], strides=[1,2,2,1])
 
 		# 2nd conv
@@ -36,7 +34,6 @@ class BayesianSegNetBasic(object):
 		# activation
 		features = keras.layers.Activation('relu')(bn)
 		# 2nd pool
-		#features, mask2 = tf_unpool.max_pool_with_argmax(features, 2)
 		features, mask2 = pooling.max_pool_with_argmax(net=features, ksize=[1,2,2,1], strides=[1,2,2,1])
 
 		# 3rd conv
@@ -63,11 +60,10 @@ class BayesianSegNetBasic(object):
 		# activation
 		features = keras.layers.Activation('relu')(bn)
 		# 4th pool
-		features, mask4 = pooling.max_pool_with_argmax(net=features, ksize=[1,2,2,1], strides=[1,2,2,1])
+		#features, mask4 = pooling.max_pool_with_argmax(net=features, ksize=[1,2,2,1], strides=[1,2,2,1])
 
 		# 1st upsample
-		#features = tf_unpool.un_max_pool(features, mask4, 2)
-		features = pooling.unpool(features, mask4, ksize=[1, 2, 2, 1])
+		#features = pooling.unpool(features, mask4, ksize=[1, 2, 2, 1])
 		# 1st 'deconv'
 		deconv1 = keras.layers.Conv2D(filters=64,
 									kernel_size=(7,7), strides=(1,1), padding='SAME',
@@ -85,7 +81,6 @@ class BayesianSegNetBasic(object):
 		deconv2 = tf.nn.dropout(x=deconv2, rate=0.5)
 		
 		# 3rd upsample
-		#features = tf_unpool.un_max_pool(deconv2, mask2, 2)
 		features = pooling.unpool(features, mask2, ksize=[1, 2, 2, 1])
 		# 3rd 'deconv'
 		deconv3 = keras.layers.Conv2D(filters=64,
@@ -93,7 +88,6 @@ class BayesianSegNetBasic(object):
 									data_format='channels_last', activation=None)(features)
 
 		# 4th upsample
-		#features = tf_unpool.un_max_pool(deconv3, mask1, 2)
 		features = pooling.unpool(features, mask1, ksize=[1, 2, 2, 1])
 		# 4th 'deconv', output classes predictions
 		deconv4 = keras.layers.Conv2D(filters=self.NUM_CLASSES,
@@ -106,11 +100,37 @@ class BayesianSegNetBasic(object):
 def loss_func(labels, logits_before_softmax):
 	''' input:	labels --- sparse labels with shape [batch, H, W], dtype = tf.uint8
 				logits_before_softmax --- logits before softmax with shape [batch, H, W, NUM_CLASSES], dtype = tf.float
-		output:	loss --- cross entropy with shape [batch, H, W], dtype = tf.float '''
+		output:	loss --- scalar cross entropy, dtype = tf.float '''
 	batch_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.to_int32(labels),
 												  logits=logits_before_softmax,
 												  name='loss')
 	# reduce the batch loss to a mean scalar
+	loss = tf.reduce_mean(batch_loss)
+	# over
+	return loss
+
+# compute loss function with rebalancing weights
+def weighted_loss_func(labels, logits_before_softmax, weights):
+	''' input:	labels --- sparse labels with shape [batch, H, W], dtype = tf.uint8
+				logits_before_softmax --- logits before softmax with shape [batch, H, W, NUM_CLASSES], dtype = tf.float
+				weights --- 1D array, dtype = tf.float32
+		output:	loss --- scalar weighted cross entropy, dtype = tf.float32 '''
+	# get the NUM_CLASSES
+	num_classes = tf.shape(weights)[0]
+	# flatten the input labels to 1D vector with length =batch*H*W
+	B = tf.shape(labels)[0]
+	H = tf.shape(labels)[1]
+	W = tf.shape(labels)[2]
+	labels = tf.reshape(labels, shape=[B*H*W])
+	# convert input sparse labels to one-hot codes (shape = [B*H*W, NUM_CLASSES])
+	labels = tf.one_hot(indices=labels, depth=num_classes, dtype=tf.float32)
+	# compute logits after softmax
+	logits = tf.nn.softmax(logits_before_softmax)
+	# reshape the logits to [B*H*W, NUM_CLASSES]
+	logits = tf.reshape(logits, shape=[B*H*W, num_classes])
+	# compute weighted cross entropy
+	batch_loss = tf.reduce_sum(-tf.multiply(labels*tf.log(logits + 1e-9), weights),axis=[1])
+	# reduce the batch loss to loss
 	loss = tf.reduce_mean(batch_loss)
 	# over
 	return loss
--- bayesian_segnet_train.py
+++ bayesian_segnet_train.py
@@ -13,6 +13,11 @@ from camvid_input import IMAGE_X
 from camvid_input import IMAGE_Y
 from camvid_input import NUM_CLASSES
 
+import CLR
+
+# set visible CUDA devices
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'		# only use one GPU is enough
+
 # constants
 train_record_path = 'CamVid/train.txt'
 test_record_path = 'CamVid/test.txt'
@@ -20,6 +25,9 @@ val_record_path = 'CamVid/val.txt'
 
 summary_path = './tensorboard/'
 summary_name = 'summary-default'    # tensorboard default summary dir
+
+model_path = './ckpts/'
+best_model_ckpt = 'best.ckpt'		# check point path
 
 train_dataset_size = 367
 test_dataset_size = 233
@@ -66,13 +74,13 @@ with tf.name_scope('input'):
 global_step = tf.Variable(initial_value=0, trainable=False, name='global_step')
 
 # build the BayesianSegNet basic model
-with tf.name_scope('segnet_basic_model'):
+with tf.name_scope('bayesian_segnet_basic_model'):
 	model = BayesianSegNetBasic(input_images, NUM_CLASSES)
 
 # train and test the model
 with tf.name_scope('train_and_test'):
-	# compute loss function (average loss in one batch)
-	loss = loss_func(labels, model.logits_before_softmax)
+	# compute loss function
+	loss = loss_func(input_labels, model.logits_before_softmax)
 	# summary the loss
 	tf.summary.scalar(name='loss', tensor=loss)
 
@@ -84,10 +92,9 @@ with tf.name_scope('train_and_test'):
 	# pixel-wise category predictions, dtype=tf.int32
 	batch_predict = tf.argmax(prob, axis=-1, output_type=tf.int32)
 	# prediction accuracy (average in one batch), dtype=tf.float32
-	batch_acc = tf.reduce_mean(tf.to_float(tf.equal(tf.to_int32(labels), batch_predict)))
+	batch_acc = tf.reduce_mean(tf.to_float(tf.equal(tf.to_int32(input_labels), batch_predict)))
 	# summary the batch accuracy
 	tf.summary.scalar(name='batch_acc', tensor=batch_acc)
-	
 
 	# optimize model parameters
 	with tf.name_scope('optimization'):
@@ -118,7 +125,7 @@ def train(cur_lr, sess, summary_writer, summary_op):
 		try:
 			# read batch of data from training set
 			labels_val, images_val = sess.run([labels, images], feed_dict={handle:train_handle_val})
-			# feed this batch to model
+			# feed this batch to AlexNet
 			_, loss_val, batch_acc_val, global_step_val, summary_buff = \
 				sess.run([train_op, loss, batch_acc, global_step, summary_op],
 						feed_dict={input_labels : labels_val,
@@ -126,7 +133,7 @@ def train(cur_lr, sess, summary_writer, summary_op):
 								   lr : cur_lr})
 			current_batch += 1
 			# print indication info
-			if current_batch % 20 == 0:
+			if current_batch % 4 == 0:
 				msg = '\tbatch number = %d, loss = %.2f, train accuracy = %.2f%%' % \
 						(current_batch, loss_val, batch_acc_val*100)
 				print(msg)
@@ -168,17 +175,17 @@ def test(sess, summary_writer):
 												input_images : images_val})
 				# update the probability prediction
 				cur_prob_pred += prob_val/test_samples
-				
+			
 			# generate the batch class prediction among all samples
 			cur_class_pred = np.argmax(cur_prob_pred, axis=-1).astype(np.uint8)
 			# compute all correct predictions
 			labels_val = labels_val.flatten().astype(np.uint8)
 			cur_class_pred = cur_class_pred.flatten()
-			correctness += np.asscalar(np.sum(a=(cur_class_pred==labels_val), dtype=np.float32))
+			correctness += np.sum(a=(cur_class_pred==labels_val), dtype=np.float32)
 		except tf.errors.OutOfRangeError:
 			break
-	# compute accuracy in epoch
-	current_acc = correctness/test_dataset_size
+	# compute accuracy and loss after a whole epoch
+	current_acc = correctness/test_dataset_size/IMAGE_X/IMAGE_Y
 	# print and summary
 	msg = 'test accuracy = %.2f%%' % (current_acc*100)
 	test_acc_summary = tf.Summary(value=[tf.Summary.Value(tag='test_accuracy',simple_value=current_acc)])
@@ -210,55 +217,107 @@ def update_learning_rate(cur_epoch):
 	return cur_lr
 
 
+# search config for CLR
+search_for_CLR = True
+# search range
+lr_min = 1e-3
+lr_max = 1e-2
+# CLR config
+epoch_size = np.ceil(train_dataset_size/train_batch_size)
+step_size = 4 * epoch_size
 
 ###################### main entrance ######################
 if __name__ == "__main__":
 	# set tensorboard summary path
+	# and load ckpt data if necessary
+	ckpt_path = ''
 	try:
-		options, args = getopt.getopt(sys.argv[1:], '', ['logdir='])
+		options, args = getopt.getopt(sys.argv[1:], '', ['logdir=', 'ckpt='])
 	except getopt.GetoptError:
 		print('invalid arguments!')
 		sys.exit(-1)
 	for option, value in options:
 		if option == '--logdir':
 			summary_name = value
-
-	# train and test the model
-	cur_lr = lr0
-	best_acc = 0
-	with tf.Session() as sess:
-		# initialize variables
-		sess.run(tf.global_variables_initializer())
-		sess.run(tf.local_variables_initializer())
-
-		# initialize IO
-		# build tf saver
-		saver = tf.train.Saver()
-		# build the tensorboard summary
-		summary_writer = tf.summary.FileWriter(summary_path+summary_name)
-		train_summary_op = tf.summary.merge_all()
-
-		# train in epochs
-		for cur_epoch in range(1, num_epochs+1):
-			# print epoch title
-			print('Current epoch No.%d, learning rate = %.2e' % (cur_epoch, cur_lr))
-			# train
-			train(cur_lr, sess, summary_writer, train_summary_op)
-			# validate
-			cur_acc = test(sess, summary_writer)
-			# update learning rate if necessary
-			cur_lr = update_learning_rate(cur_epoch)
-
-			if cur_acc > best_acc:
-				# save check point
-				saver.save(sess=sess,save_path=model_path+best_model_ckpt)
-				# print message
-				print('model improved, save the ckpt.')
-				# update best loss
-				best_acc = cur_acc
+		elif option == '--ckpt':
+			ckpt_path = value
+	
+	if not search_for_CLR:
+		# train and test the model
+		best_acc = 0
+		with tf.Session() as sess:
+			# build tf saver
+			saver = tf.train.Saver()
+			# load model if necessary
+			if len(ckpt_path)>0:
+				saver.restore(sess, tf.train.latest_checkpoint(ckpt_path))
 			else:
-				# print message
-				print('model not improved.')
-	# finished
-	print('++++++++++++++++++++++++++++++++++++++++')
-	print('best accuracy = %.2f%%.'%(best_acc*100))
+				# initialize variables
+				sess.run(tf.global_variables_initializer())
+
+			# initialize IO
+			sess.run(tf.local_variables_initializer())
+			# build the tensorboard summary
+			summary_writer = tf.summary.FileWriter(summary_path+summary_name)
+			train_summary_op = tf.summary.merge_all()
+
+			# train in epochs
+			for cur_epoch in range(1, num_epochs+1):
+				# compute current lr
+				cur_lr = CLR.clr(epoch=cur_epoch, epoch_size=epoch_size, step_size=step_size,
+								 lr_min=lr_min, lr_max=lr_max)
+				# print epoch title
+				print('Current epoch No.%d, learning rate = %.2e' % (cur_epoch, cur_lr))
+				# train
+				train(cur_lr, sess, summary_writer, train_summary_op)
+				# validate
+				cur_acc = test(sess, summary_writer)
+
+				if cur_acc > best_acc:
+					# save check point
+					saver.save(sess=sess,save_path=model_path+best_model_ckpt)
+					# print message
+					print('model improved, save the ckpt.')
+					# update best loss
+					best_acc = cur_acc
+				else:
+					# print message
+					print('model not improved.')
+		# finished
+		print('++++++++++++++++++++++++++++++++++++++++')
+		print('best accuracy = %.2f%%.'%(best_acc*100))
+	else:
+		# find the lr_min and lr_max for CLR strategy
+		# CLR config
+		step_size = num_epochs * epoch_size
+		# start
+		with tf.Session() as sess:
+			# load model if necessary
+			if len(ckpt_path)>0:
+				saver.restore(sess, tf.train.latest_checkpoint(ckpt_path))
+			else:
+				# initialize variables
+				sess.run(tf.global_variables_initializer())
+
+			# initialize IO
+			sess.run(tf.local_variables_initializer())
+			# build the tensorboard summary
+			summary_writer = tf.summary.FileWriter(summary_path+summary_name)
+			train_summary_op = tf.summary.merge_all()
+
+			# train in epochs
+			for cur_epoch in range(1, num_epochs+1):
+				# compute current lr
+				cur_lr = CLR.clr(epoch = cur_epoch,
+								 epoch_size = epoch_size, step_size = step_size, 
+								 lr_min = lr_min,
+								 lr_max = lr_max)
+				# print epoch title
+				print('Current epoch No.%d, learning rate = %.2e' % (cur_epoch, cur_lr))
+				# train
+				train(cur_lr, sess, summary_writer, train_summary_op)
+				# validate
+				cur_acc = test(sess, summary_writer)
+
+		# finished
+		print('++++++++++++++++++++++++++++++++++++++++')
